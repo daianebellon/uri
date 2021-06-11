312e55de075d166895e359162afa969a05599278
daiane.bellon <daianebellon@hotmail.com>
2021-06-11 16:06:09 -0300

    First commit

---
+++ src/iniciante/Uri1000.java
@@ -0,0 +1,8 @@
+package iniciante;
+
+public class Uri1000 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/iniciante/Uri1001.java
@@ -0,0 +1,14 @@
+package iniciante;
+
+import java.util.Scanner;
+
+public class Uri1001 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        System.out.println("X = " + (a + b));
+
+    }
+}
